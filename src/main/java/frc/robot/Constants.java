7320feab5bff77d459993d4b9b329a343009d1d0
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2025-01-11 12:38:12 -0600

    Removed unnecessary Constants.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,10 +7,6 @@ package frc.robot;
 import edu.wpi.first.math.util.Units;
 
 public class Constants {
-  public static final int kMotorPort = 0;
-  public static final int kEncoderAChannel = 0;
-  public static final int kEncoderBChannel = 1;
-  public static final int kJoystickPort = 0;
 
   public static final double kElevatorKp = 5;
   public static final double kElevatorKi = 0;
@@ -25,13 +21,8 @@ public class Constants {
   public static final double kElevatorDrumRadius = Units.inchesToMeters(2.0);
   public static final double kCarriageMass = 4.0; // kg
 
-  public static final double kSetpointMeters = 30.0;
   // Encoder is reset to measure 0 at the bottom, so minimum height is 0.
   public static final double kMinElevatorHeightMeters = 0.0;
   public static final double kMaxElevatorHeightMeters = 10.25;
 
-  // distance per pulse = (distance per revolution) / (pulses per revolution)
-  //  = (Pi * D) / ppr
-  public static final double kElevatorEncoderDistPerPulse =
-      2.0 * Math.PI * kElevatorDrumRadius / 4096;
 }
